37c470bbc6f9493d2244129f140309f087b3acbd
Koudela <main.office.koudela.net@gmail.com>
2016-09-06 09:48:06 +0200

    introduce SudokuGroups.java

--- main/java/net/koudela/sudoku/MainActivity.java
+++ main/java/net/koudela/sudoku/MainActivity.java
@@ -22,17 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected View requestView;
     protected Button[] mainButtons = new Button[DIM * DIM];
     protected TextView[] helperTextViews = new TextView[DIM*DIM];
-    protected int[][] sudokuGroups = {
-            {0,1,2,9,10,11,18,19,20},
-            {27,28,29,36,37,38,45,46,47},
-            {54,55,56,63,64,65,72,73,74},
-            {3,4,5,12,13,14,21,22,23},
-            {30,31,32,39,40,41,48,49,50},
-            {57,58,59,66,67,68,75,76,77},
-            {6,7,8,15,16,17,24,25,26},
-            {33,34,35,42,43,44,51,52,53},
-            {60,61,62,69,70,71,78,79,80}
-    };
     protected int arrIdEasyTouchButton = 0;
 
     @Override
@@ -202,8 +191,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void setAutoHints (int arr_id, String value) {
         int ii = arr_id / DIM;
         int jj = arr_id % DIM;
-        int[] sudokuGroup = getSudokuGroup(arr_id);
-        for (int arrId :sudokuGroup) {
+        for (int arrId :SudokuGroups.getGroupedGroup(arr_id)) {
             setHint(arrId, value, false);
         }
         for (int i=0; i<DIM; i++) {
@@ -214,14 +202,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             arr_id = ii * DIM + j;
             setHint(arr_id, value, false);
         }
-    }
-    protected int[] getSudokuGroup(int arr_id) {
-        for (int groupId=0; groupId<DIM; groupId++) {
-            for (int index=0; index<DIM; index++) {
-                if (sudokuGroups[groupId][index] != arr_id) continue;
-                return sudokuGroups[groupId];
-            }
-        }
-        return sudokuGroups[-1];
     }
 }
