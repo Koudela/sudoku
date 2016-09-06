b3d73e654ca5c09eef35e2eebd310a26e600737e
Koudela <main.office.koudela.net@gmail.com>
2016-09-06 07:55:36 +0200

    add development options in preferences

--- main/java/net/koudela/sudoku/PreferencesFragment.java
+++ main/java/net/koudela/sudoku/PreferencesFragment.java
@@ -11,6 +11,10 @@ public class PreferencesFragment extends PreferenceFragment implements SharedPre
     public static final String KEY_PREF_FONT_SIZE_MAIN = "pref_font_size_main";
     public static final String KEY_PREF_FONT_SIZE_HELPER = "pref_font_size_helper";
     public static final String KEY_PREF_FONT_SIZE_INPUT = "pref_font_size_input";
+    public static final String KEY_PREF_AUTO_HINT_ADV1 = "pref_auto_hint_adv1";
+    public static final String KEY_PREF_AUTO_HINT_ADV2 = "pref_auto_hint_adv2";
+    public static final String KEY_PREF_AUTO_INSERT1 = "pref_auto_insert1";
+    public static final String KEY_PREF_AUTO_INSERT2 = "pref_auto_insert2";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,18 @@ public class PreferencesFragment extends PreferenceFragment implements SharedPre
                 break;
             case (PreferencesFragment.KEY_PREF_FONT_SIZE_INPUT):
                 Log.v(key, sharedPreferences.getString(key, ""));
+                break;
+            case (PreferencesFragment.KEY_PREF_AUTO_HINT_ADV1):
+                Log.v(key, "" + sharedPreferences.getBoolean(key, false));
+                break;
+            case (PreferencesFragment.KEY_PREF_AUTO_HINT_ADV2):
+                Log.v(key, "" + sharedPreferences.getBoolean(key, false));
+                break;
+            case (PreferencesFragment.KEY_PREF_AUTO_INSERT1):
+                Log.v(key, "" + sharedPreferences.getBoolean(key, false));
+                break;
+            case (PreferencesFragment.KEY_PREF_AUTO_INSERT2):
+                Log.v(key, "" + sharedPreferences.getBoolean(key, false));
                 break;
         }
     }
